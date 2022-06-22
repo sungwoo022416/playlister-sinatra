920b6f2d94bc0287caefb41ed55010befabc9bdd
Sungwoo Kim <sungwoo.kim@yale.edu>
2022-06-22 14:19:00 -0700

    still needs work

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,14 @@
+class SongsController < ApplicationController
+    
+    get '/songs' do
+        @songs = Song.all
+        erb :"song/index"
+    end
+        
+    get '/songs/:slug' do
+        @song = Song.find_by_slug(params[:slug])
+        erb :"song/show"
+    end
+
+
+end

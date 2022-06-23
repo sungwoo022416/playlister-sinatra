821e9ef467a955075ce0d78666b9703488fc20dc
Sungwoo Kim <sungwoo.kim@yale.edu>
2022-06-23 07:53:39 -0700

    unfinished

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -21,7 +21,20 @@ class SongsController < ApplicationController
         @artist = @song.artist
         @genre = @song.genres
         erb :"song/show"
+    end
 
+    get '/songs/:slug/edit' do
+        @song = Song.find_by_slug(params[:slug])
+        erb :"song/edit"
+    end
+
+    patch '/songs/:slug' do
+        @song = Song.find_by_slug(params[:slug])
+        @song.artist = Artist.find_or_create_by(params[:artist])
+        @song.genres_ids = (params[:genres])
+        @song.update(params[:song])
+        
+        redirect "/songs/#{@song.slug}"
     end
 
 

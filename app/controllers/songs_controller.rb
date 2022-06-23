ccd763d3553208442aee94efa74dde4a4f803d72
Sungwoo Kim <sungwoo.kim@yale.edu>
2022-06-23 07:17:31 -0700

    .

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,7 +4,18 @@ class SongsController < ApplicationController
         @songs = Song.all
         erb :"song/index"
     end
-        
+
+    get '/songs/new' do
+        erb :"song/new"
+    end
+
+    post '/songs' do
+        @song= Song.create(params[:song])
+        @song.artist = Artist.find_or_create_by(params[:artist])
+        @song.genres_ids = (params[:genres])
+        redirect "/songs/#{@song.slug}"
+    end
+
     get '/songs/:slug' do
         @song = Song.find_by_slug(params[:slug])
         @artist = @song.artist
